a5e5c24ab8a9b9a7528aba239a35bcd356c36a46
Felipe Sforcini <felipesforcini@MacBook-Air-de-Felipe.local>
2023-03-01 03:02:05 -0300

    header done

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,15 @@
+import './styles.scss';
+
 export default function Header() {
     return (
-        <div className="container__header">
-            <div>
-                <span>Menu</span>
-                <span>Felipe Sforcini</span>
+        <div className="container-header">
+            <div className='container-header__items'>
+                <span className='container-header__item pointer'>Menu</span>
+                <span className='container-header__item'>Felipe Sforcini</span>
+            </div>
+
+            <div className='container-header__items'>
+                <span className='container-header__item'>Step 3 Avaliação Técnica All Tax Platform</span>
             </div>
         </div>
     )
